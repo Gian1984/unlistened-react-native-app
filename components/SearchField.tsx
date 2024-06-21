50c3f49ad4a811844488dfb4fa152b4fff563166
Gianluca Tiengo <gl.tiengo@gmail.com>
2024-06-21 16:18:08 +0200

    first commit

---
+++ components/SearchField.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+type SearchProps = {
+    onSearch: (query: string) => void;
+};
+
+const Search: React.FC<SearchProps> = ({ onSearch }) => {
+    const [query, setQuery] = useState('');
+
+    const handleSearch = () => {
+        onSearch(query);
+    };
+
+    return (
+        <View style={styles.searchContainer}>
+            <Ionicons name="search" size={24} color="gray" />
+            <TextInput
+                style={styles.input}
+                placeholder="Search podcasts"
+                value={query}
+                onChangeText={setQuery}
+                onSubmitEditing={handleSearch}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+        borderRadius: 25,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        marginHorizontal: 10,
+        marginVertical: 5,
+    },
+    input: {
+        flex: 1,
+        marginLeft: 10,
+    },
+});
+
+export default Search;
